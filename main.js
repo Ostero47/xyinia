f70d8986cf49cfa102babbf6caf1c78efde25acd
Ostero47 <kasymilia123@gmail.com>
2025-12-03 11:48:01 +0300

    .

---
+++ main.js
@@ -0,0 +1,38 @@
+// alert("задание 1") ДЗ ПО ИП
+// let qwe =10
+// alert(qwe)
+// qwe =20
+// alert(qwe)
+
+// alert("задание 2")
+// let qwe =10
+// let ewq =2
+// alert(qwe+ewq)
+// alert(qwe-ewq)
+// alert(qwe*ewq)
+// alert(qwe/ewq)
+
+// alert("задание 3")
+// let qwe =10
+// let ewq =5
+// const result=qwe+ewq
+// alert(result)
+
+// alert("задание 4")
+// let qwe =1
+// let ewq =2
+// let rty =3
+// alert(qwe+ewq+rty)
+
+// alert("задание 5")
+// let a=10
+// let b=5
+// const c=a+b
+// alert(c)
+// let d=7
+// alert(c+d)
+
+
+
+
+
